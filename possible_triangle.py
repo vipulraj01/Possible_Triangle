995df96f6747f11322bf8f78febe8d0fcff68c12
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2022-10-04 20:33:26 +0530

    Update possible_triangle.py

--- possible_triangle.py
+++ possible_triangle.py
@@ -1,6 +1,7 @@
 a = int(input("Enter the first side : "))
 b = int(input("Enter the second side : "))
 c = int(input("Enter the third side : "))
+# if a + b is less than or equal to b + a and b - b
 if a+b>=c and b+c>=a and c+a>=b:
     print("Triangle is possible")
 else:
